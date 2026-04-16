650b7d78f7826846dd0780e1e7ff3cec140da02b
Brian  Doyle <briandoyle81@gmail.com>
2026-04-16 12:39:33 -0400

    Add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,6 +23,22 @@ const siteUrl =
 
 export const metadata: Metadata = {
   metadataBase: new URL(siteUrl),
+  manifest: "/favicon/site.webmanifest",
+  icons: {
+    icon: [
+      { url: "/favicon/favicon.ico" },
+      { url: "/favicon/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    ],
+    shortcut: ["/favicon/favicon.ico"],
+    apple: [
+      {
+        url: "/favicon/apple-touch-icon.png",
+        sizes: "180x180",
+        type: "image/png",
+      },
+    ],
+  },
   title: {
     default: "Void Tactics",
     template: "%s | Void Tactics",
